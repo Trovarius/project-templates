df0983fac6330234faa66463819de96089efa94c
Trovairus <tordek.coast@gmail.com>
2020-04-12 14:42:38 +0100

    fetch data with react hooks

--- src/node/react-hooks/src/pages/components/hello.tsx
+++ src/node/react-hooks/src/pages/components/hello.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React,  { useState, useEffect } from "react";
 
 interface HelloProps { 
     compiler: string; 
@@ -7,6 +7,11 @@ interface HelloProps {
 
 export const Hello = (props: HelloProps) => <h1>Hello from darknest teste {props.compiler} and {props.framework}!</h1>;
 
+interface Post {
+    title: string,
+    author: string
+    id: number
+}
 
 export const Button: React.FC = () => {
     const [value, setValue] = React.useState(0);
@@ -15,8 +20,34 @@ export const Button: React.FC = () => {
         setValue(value + 1)
     }
 
+    useEffect(() =>{
+        document.title = `You clicked ${value} times`;
+    })
+
     return (<div>
         <Hello compiler="Teste" framework={value.toString()}></Hello>
+        <DataLoader>
+            { (data: Array<Post>) => data.map((x: Post) => <Hello compiler={x.title} framework={x.author}></Hello>)}
+        </DataLoader>
         <button onClick={onClick}>Click here to add</button>
+    </div>)
+}
+
+export function DataLoader(props : any){
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/posts")
+        .then(response => response.json())
+        .then(data => {
+            setData(data);
+            console.log(data);
+            setIsLoading(false);
+        });
+    }, []);
+
+    return (<div>
+         { isLoading ? "Loading" : props.children(data)}
     </div>)
 }
